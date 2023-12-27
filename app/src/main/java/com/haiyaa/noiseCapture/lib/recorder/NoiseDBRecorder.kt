d909ea41bf5e83a5f3d3a37009208a99817418c5
sobol <C109152304@nkust.edu.tw>
2023-12-27 19:18:41 +0800

    整理程式

--- app/src/main/java/com/haiyaa/noiseCapture/lib/recorder/NoiseDBRecorder.kt
+++ app/src/main/java/com/haiyaa/noiseCapture/lib/recorder/NoiseDBRecorder.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.media.MediaRecorder
 import android.os.Build
 import android.os.Handler
+import android.os.Looper
 import android.os.Message
 import android.util.Log
 import androidx.compose.runtime.MutableState
@@ -15,20 +16,22 @@ class NoiseDBRecorder(private val context: Context, val DB: MutableState<Double>
 
     var db: Double = 0.0
 
-    val handlerMeasure = object : Handler() {
+    val handlerMeasure = object : Handler(Looper.myLooper()!!) {
         override fun handleMessage(msg: Message) {
             when (msg.what) {
                 1 -> {
                     val amp = recorder?.maxAmplitude
                     // 公式：Gdb = 20log10(V1/V0)
                     // Google已提供方法幫你取得麥克風的檢測電壓(V1)以及參考電壓(V0)
-                    db = 20 * (amp!!.let { Math.abs(it.toDouble()) }!!.let { Math.log10(it) })
+                    db = 20 * (amp!!.let { Math.abs(it.toDouble()) }.let { Math.log10(it) })
                     Log.d("6", db.toString())
 
                     // if -Infinity
                     if (db == Double.NEGATIVE_INFINITY) {
                         db = 0.0
                     }
+
+                    DB.value=db
                 }
             }
             super.handleMessage(msg)
@@ -39,9 +42,8 @@ class NoiseDBRecorder(private val context: Context, val DB: MutableState<Double>
         override fun run() {
             handlerMeasure.sendEmptyMessage(1)
 
-            //每500毫秒抓取一次檢測結果
-            handlerMeasure.postDelayed(this, 500)
-            DB.value=db
+            //每1秒抓取一次檢測結果
+            handlerMeasure.postDelayed(this, 1000)
         }
     }
 
@@ -57,7 +59,6 @@ class NoiseDBRecorder(private val context: Context, val DB: MutableState<Double>
         createRecorder().apply {
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-//            setOutputFile("/dev/null")
             val tmpRecordingFolder = File(context.filesDir, "tmp_media")
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 setOutputFile(tmpRecordingFolder)
